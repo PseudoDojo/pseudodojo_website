a37bfb64a451b5b24048584cf1637212ab285255
Matteo Giantomassi <gmatteo@gmail.com>
2026-02-13 11:54:14 +0100

    Pass list of dataclass to template for better flexibility

--- html_tools.py
+++ html_tools.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import json
+import dataclasses
 import plotly.io as pio
 
 from jinja2 import Environment
@@ -26,10 +27,16 @@ TEMPLATE = env.from_string("""
 
 <h1>{{ title }}</h1>
 
-{% for text, fig in zip(oncv_texts, oncv_figures) %}
-  <h2>{{ text }}</h2>
+<!-- Show input file -->
+<h2>oncvpsp input</h2>
+<pre><code> {{ input_str | e }} </code></pre>
+
+<!-- Show list of plots -->
+{% for plot in plots %}
+  <h2>{{ plot.title }}</h2>
+  <p>{{ plot.text }}</p>
   <div class="plot">
-    {{ fig | safe }}
+    {{ plot.html | safe }}
   </div>
 {% endfor %}
 
@@ -37,6 +44,14 @@ TEMPLATE = env.from_string("""
 </html>
 """
 )
+
+
+@dataclasses.dataclass(kw_only=True)
+class PlotData:
+    """Collect info on a figure passed to the jinja2 template."""
+    html: str
+    title: str
+    text: str
 
 
 def write_html_from_oncvpsp_outpath(out_path: str) -> str:
@@ -57,47 +72,74 @@ def write_html_from_oncvpsp_outpath(out_path: str) -> str:
     if plotter is None:
         raise RuntimeError(f"Cannot build plotter from {out_path=}")
 
+    def to_html(fig):
+        """
+        Convert from plotly figure to html that will then be included in the HTML page using Jinja2 template.
+        Note: plotly.js is loaded in the template from CDN to reduce file size.
+        """
+        return pio.to_html(fig, include_plotlyjs=False, full_html=False)
+
+    plots = []
+    app = plots.append
+
     # Use plotly True to generate a matplotlib figure and convert it to plotly automatically.
-    oncv_figures, oncv_texts =  [], []
     kwargs = dict(show=False, plotly=True)
 
     # This won't work for Si, I have to fix it at the abipy level.
-    oncv_texts.append("These are the radial wavefunctions")
-    oncv_figures.append(plotter.plot_radial_wfs(**kwargs))
+    app(PlotData(
+        html=to_html(plotter.plot_radial_wfs(**kwargs)),
+        title="AE and PS radial wavefunctions",
+        text="These are the radial wavefunctions",
+    ))
 
-    oncv_texts.append("These are the famous ATAN LOGDERs signaling the presence of ghost states ...")
-    oncv_figures.append(plotter.plot_atan_logders(**kwargs))
+    app(PlotData(
+        html=to_html(plotter.plot_atan_logders(**kwargs)),
+        title="Arctan of the logarithmic derivatives",
+        text="These are the famous ATAN LOGDERs signaling the presence of ghost states ...",
+    ))
 
-    oncv_texts.append("kene_vs_ecut ...")
-    oncv_figures.append(plotter.plot_kene_vs_ecut(**kwargs))
+    app(PlotData(
+        html=to_html(plotter.plot_kene_vs_ecut(**kwargs)),
+        title="Convergence in  G-space estimated by ONCVPSP",
+        text="kene_vs_ecut ...",
+    ))
 
-    oncv_texts.append("These are the projectors")
-    oncv_figures.append(plotter.plot_projectors(**kwargs))
+    app(PlotData(
+        html=to_html(plotter.plot_projectors(**kwargs)),
+        title="Projectors",
+        text="These are the projectors",
+    ))
 
-    oncv_texts.append("These are the potentials")
-    oncv_figures.append(plotter.plot_potentials(**kwargs))
+    app(PlotData(
+        html=to_html(plotter.plot_potentials(**kwargs)),
+        title="Local potential and l -dependent potentials",
+        text="These are the potentials",
+    ))
 
-    oncv_texts.append("These are the densities")
-    oncv_figures.append(plotter.plot_densities(**kwargs))
+    app(PlotData(
+        html=to_html(plotter.plot_densities(**kwargs)),
+        title="Core-Valence-Model charge densities",
+        text="These are the densities",
+    ))
 
-    # This only for Meta-gga pseudos.
+    # This only for meta-gga pseudos.
     if parser.is_metapsp:
-        oncv_texts.append("This is tau")
-        oncv_figures.append(plotter.plot_tau(**kwargs))
+        app(PlotData(
+            html=to_html(plotter.plot_tau(**kwargs)),
+            title="Kinetic energy density",
+            text="This is tau",
+        ))
 
-        oncv_texts.append("This is vtau")
-        oncv_figures.append(plotter.plot_vtau(**kwargs))
-
-    # Convert from plotly to html that will then be included in the HTML page using Jinja2 template.
-    # Note: plotly.js is loaded in the template from CDN to reduce file size.
-    oncv_figures = [pio.to_html(fig, include_plotlyjs=False, full_html=False) for fig in oncv_figures]
-    #oncv_figures = [pio.to_html(fig, include_plotlyjs=True, full_html=False) for fig in oncv_figures]
+        app(PlotData(
+            html=to_html(plotter.plot_vtau(**kwargs)),
+            title="Meta-GGA potential",
+            text="This is vtau",
+        ))
 
     # Here we read the json file with the validation results and produce plotly plots.
     # Note that this step is optional as a pseudo migth not have validation results.
     # TODO: Here we need some machinery to produce plotly files from the data read from the JSON file.
     # and the figures should be then injected into the template.
-    results_figures, results_texts = [], []
 
     json_path = out_path.replace(".out", ".djson", 1) # TODO json or djson?
     if os.path.exists(json_path):
@@ -105,16 +147,12 @@ def write_html_from_oncvpsp_outpath(out_path: str) -> str:
             results = json.load(fh)
         raise NotImplementedError(f"Don't know how to produce plotly figures from {json_path=}")
 
-
-    if len(oncv_figures) != len(oncv_texts):
-        raise RuntimeError(f"{len(oncv_figures)=} != {len(oncv_texts)=}")
-
     name = os.path.basename(out_path).replace(".out", "")
 
     html = TEMPLATE.render(
         title=f"Oncvpsp figures for {name} ",
-        oncv_figures=oncv_figures,
-        oncv_texts=oncv_texts,
+        input_str=parser.get_input_str(),
+        plots=plots,
     )
 
     # Write the HTML file.
@@ -126,11 +164,14 @@ def write_html_from_oncvpsp_outpath(out_path: str) -> str:
 
 
 if __name__ == "__main__":
-    # This module can be executed as a standalone script to facilitate debugging
-    # Syntax: `python html_tools.py Si.out`
     import sys
-    import webbrowser
+    if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
+        print("This module can be executed as a standalone script to facilitate developments")
+        print("Syntax: `python html_tools.py Si.out`")
+        sys.exit(1)
+
     from pathlib import Path
+    import webbrowser
     html_path = write_html_from_oncvpsp_outpath(sys.argv[1])
     print(f"Opening {html_path} in browser...")
     webbrowser.open(Path(html_path).resolve().as_uri())
